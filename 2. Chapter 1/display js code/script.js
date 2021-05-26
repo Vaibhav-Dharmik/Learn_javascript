0297c14ea23572d3cd91a014abf4bf02416351ef
Vaibhav Sanjayrao Dharmik <55710097+Vaibhav-Dharmik@users.noreply.github.com>
2021-05-26 22:58:13 +0530

    external script

---
+++ 2. Chapter 1/display js code/script.js
@@ -0,0 +1,2 @@
+// we can write code here without <script></script> tag
+document.getElementById('box_external').innerHTML = "Lets go, with javaScript: from document.getElementById('').innerHTML('') external file"
